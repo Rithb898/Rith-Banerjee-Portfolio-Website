c63a71d848a44ccb080379db8ae037508b5402fa
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2024-11-25 21:18:21 +0530

    change header bg and border

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-20">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <nav className="flex gap-1 p-0.5 border border-white/50 md:border-white/15 rounded-full bg-white/30 md:bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">
           Home
         </a>

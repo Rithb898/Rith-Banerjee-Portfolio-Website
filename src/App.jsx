70bce0e9a41f187ce975c60939e78593f1c12b08
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2024-11-25 21:26:30 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
 import Header from './sections/Header'
-import Hero from './sections/Hero'
 import Projects from './sections/Projects'
 import Tape from './sections/Tape'
 import About from './sections/About'
-import { Hero1 } from './sections/Hero1'
-// import reactLogo from './assets/react.svg'
+import { Hero } from './sections/Hero'
 
 function App() {
 
   return (
     <>
       <Header />
-      {/* <Hero /> */}
-      <Hero1 />
+      <Hero />
       <Projects />
       <Tape />
       <About />
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,115 +1,154 @@
-import React from "react";
-import memojiSmile from "../assets/images/memoji-smile.png";
-import grainImage from "../assets/images/grain.jpg";
-import HeroOrbit from "../components/HeroOrbit";
-import StarIcon from "../assets/icons/star.svg?react";
-import SparkleIcon from "../assets/icons/sparkle.svg?react";
+import React, { useEffect, useRef } from "react";
+import { FiArrowRight } from "react-icons/fi";
+import profilePic from "../assets/profilepic.png";
+import reactlogo from "../assets/react.png";
+import tailwindCssLogo from "../assets/tailwindcss.png";
+import html from "../assets/html.png";
+import js from "../assets/js.png";
 
-function Hero() {
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
+
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+
+export const Hero = () => {
+  const color = useMotionValue(COLORS_TOP[0]);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(150% 150% at 50% 0%, #020617 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
-        <div
-          className="absolute inset-0 -z-30 opacity-5"
-          style={{ backgroundImage: `url(${grainImage})` }}
-        ></div>
+    <motion.section
+      id="home"
+      style={{
+        backgroundImage,
+      }}
+      className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200"
+      ref={ref}
+    >
+      <motion.div
+        className="hidden md:block absolute left-[140px] top-[90px] z-[99999]"
+        drag
+        dragConstraints={ref}
+        whileDrag={{ scale: 1.2 }}
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+      >
+        <img
+          src={reactlogo}
+          height="170"
+          width="190"
+          alt="cursor"
+          className=""
+          draggable="false"
+        />
+      </motion.div>
 
-        <div className="size-[620px] hero-ring"></div>
-        <div className="size-[820px] hero-ring"></div>
-        <div className="size-[1020px] hero-ring"></div>
-        <div className="size-[1220px] hero-ring"></div>
+      <motion.div
+        className="hidden md:block absolute right-[140px] top-[110px] z-[99999]"
+        drag
+        dragConstraints={ref}
+        whileDrag={{ scale: 1.2 }}
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+      >
+        <img
+          src={html}
+          height="140"
+          width="150"
+          alt="cursor"
+          className=""
+          draggable="false"
+        />
+      </motion.div>
 
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-300" />
-        </HeroOrbit>
+      <motion.div
+        className="hidden md:block absolute left-[120px] bottom-[200px] z-[99999]"
+        drag
+        dragConstraints={ref}
+        whileDrag={{ scale: 1.2 }}
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+      >
+        <img
+          src={js}
+          height="210"
+          width="220"
+          alt="cursor"
+          className=""
+          draggable="false"
+        />
+      </motion.div>
 
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-300" />
-        </HeroOrbit>
+      <motion.div
+        className="hidden md:block absolute right-[120px] bottom-[200px] z-[999999]"
+        drag
+        dragConstraints={ref}
+        whileDrag={{ scale: 1.2 }}
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+      >
+        <img
+          src={tailwindCssLogo}
+          height="190"
+          width="200"
+          alt="cursor"
+          className=""
+          draggable="false"
+        />
+      </motion.div>
 
-        <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-emerald-300" />
-        </HeroOrbit>
-
-        <HeroOrbit size={430} rotation={-14}>
-          <SparkleIcon className="size-8 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={440} rotation={79}>
-          <SparkleIcon className="size-5 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={530} rotation={178}>
-          <SparkleIcon className="size-10 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={710} rotation={144}>
-          <SparkleIcon className="size-14 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={720} rotation={85}>
-          <div className="size-3 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={520} rotation={-41}>
-          <div className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={650} rotation={-5}>
-          <div className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-      </div>
-      <div className="container">
-        <div className="flex flex-col items-center">
-          <img
-            src={memojiSmile}
-            alt="Person smiling"
-            className="size-[100px]"
-          />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full relative"><div className="bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div></div>
-            <div className="text-sm font-semibold">
-              Available for new projects
-            </div>
+      <div className="relative z-10 flex flex-col items-center md:bg-black md:rounded-full md:py-20 md:px-28">
+        <span className="mb-3 flex items-center gap-3 rounded-full bg-gray-600/50 px-5 py-2 text-lg font-semibold">
+          <div className="bg-green-500 size-2.5 rounded-full relative">
+            <div className="bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div>
           </div>
-        </div>
-
-        <div className="max-w-lg mx-auto">
-          <h1 className="font-calistoga text-3xl md:text-5xl font-bold text-center mt-8 tracking-wide">
-            Building Excepcional User Experiences
-          </h1>
-          <p className="mt-4 text-center text-white/60 md:text-lg">
-            I'm a frontend software developer focused on building exceptional
-            digital experiences.
-          </p>
-        </div>
-
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 py-3 h-12 rounded-xl">
-            <span className="font-semibold">Explore My Work</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-4"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
-              />
-            </svg>
-          </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
-            <span>👋</span>
-            <span className="font-semibold ">Let's Connect</span>
-          </button>
-        </div>
+          Open for Work
+        </span>
+        <h1 className="max-w-3xl text-white bg-clip-text text-center text-4xl font-bold leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
+          Hi, I am
+        </h1>
+        <h1 className="max-w-3xl text-white tracking-widest md:tracking-normal md:bg-gradient-to-br from-white to-gray-700 bg-clip-text text-center text-4xl font-bold leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight font-calistoga">
+          Rith Banerjee
+        </h1>
+        <img src={profilePic} alt="profile pic" width={250} />
+        <p className="my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
+          Front-end Developer based in Kolkata, India. I have a passion for
+          creating engaging and user-friendly web experiences.
+        </p>
+        <a
+          href="https://drive.google.com/file/d/1egpqZdxNkBim9y4fyMw0qBcDFfml8dnl/view?usp=sharing"
+          target="_blank"
+        >
+          <motion.button
+            style={{
+              border,
+              boxShadow,
+            }}
+            whileHover={{
+              scale: 1.05,
+            }}
+            whileTap={{
+              scale: 0.985,
+            }}
+            className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50 text-lg font-semibold"
+          >
+            Download Resume
+            <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+          </motion.button>
+        </a>
       </div>
-    </div>
+    </motion.section>
   );
-}
-
-export default Hero;
+};
